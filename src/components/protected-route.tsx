848f9294a0c5cb8d340f6080e3f091da3198532f
yui62yui <you@example.com>
2023-10-22 00:37:21 +0900

    #2.3 Protected Routes

---
+++ src/components/protected-route.tsx
@@ -0,0 +1,15 @@
+import { auth } from "../firebase";
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = auth.currentUser;
+  if (user === null) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+}
--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,5 +1,12 @@
+import {
+  createUserWithEmailAndPassword,
+  updateCurrentUser,
+  updateProfile,
+} from "firebase/auth";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { auth } from "../firebase";
 
 const Wrapper = styled.div`
   height: 100%;
@@ -39,6 +46,7 @@ const Input = styled.input`
 `;
 
 export default function CreateAccount() {
+  const navigate = useNavigate();
   const [isLoading, setLoading] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -56,9 +64,21 @@ export default function CreateAccount() {
       setPassword(value);
     }
   };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (isLoading || name === "" || email === "" || password === "") return;
     try {
+      setLoading(true);
+      const credentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(credentials.user);
+      await updateProfile(credentials.user, {
+        displayName: name,
+      });
+      navigate("/");
     } catch (e) {
       // setError
     } finally {
@@ -67,7 +87,7 @@ export default function CreateAccount() {
   };
   return (
     <Wrapper>
-      <Title>Log into X</Title>
+      <Title>Join X</Title>
       <Form onSubmit={onSubmit}>
         <Input
           onChange={onChange}
